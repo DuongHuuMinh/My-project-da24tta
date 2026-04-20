9cffaa733fa3333be07811ba8d2786748008a043
minh <huuminh466@gmail.com>
2026-04-20 10:11:26 +0700

    updeta all, change html/page4,5 and js/lib4,5

---
+++ js/lib4.js
@@ -0,0 +1,12 @@
+
+let soLuong = document.getElementsByClassName("sl");
+let donGia = document.getElementsByClassName("dg");
+let thanhtien = document.getElementsByClassName("tht");
+let tong = 0;
+for(let i=0 ; i<soLuong.length ; i++)
+{
+    thanhtien[i].innerHTML = parseInt(soLuong[i].innerHTML) * parseInt(donGia[i].innerHTML);
+    tong = parseInt(tong) + parseInt(thanhtien[i].innerHTML);
+};
+document.getElementById("Tgt").innerHTML=tong;
+
---
+++ js/lib5.js
@@ -0,0 +1,5 @@
+function inBangCC(){
+    let myForm = document.forms["forminCC"];
+    let num = myForm.eletment[0].innerHTML;
+}
+
